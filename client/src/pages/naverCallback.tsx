447557c12f6af47515f6bc53b78130a92ffdc2e2
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2024-05-29 21:39:27 +0900

    부모창 수정

--- client/src/pages/naverCallback.tsx
+++ client/src/pages/naverCallback.tsx
@@ -14,8 +14,10 @@ export default function NaverCallback() {
         state: state,
       })
       .then((response) => {
+        console.log('Login response:', response);
         //spring에서 발급된 jwt 반환 localStorage 저장
         localStorage.setItem('accessToken', response.headers.accesstoken);
+        console.log('Access token set in localStorage');
         // / 페이지에 메시지 보내기
         console.log('Sending message to opener:', window.opener);
         if (window.opener) {
@@ -26,12 +28,18 @@ export default function NaverCallback() {
         }
 
         // 팝업창 닫기
+        console.log('Closing window');
         window.close();
       })
       .catch((err) => {
         //에러발생 시 경고처리 후 login 페이지로 전환
         alert(err.response.data.detail);
-        window.location.href = '/login';
+        localStorage.setItem('loginStatus', 'fail');
+        if (window.opener) {
+          window.opener.postMessage('loginFail', window.location.origin);
+        }
+        console.log('Login status set to fail');
+        window.close();
       });
   }, []);
 
