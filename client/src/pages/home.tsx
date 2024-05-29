0d3f28927c297f819dfd623745d74a7512f50d2c
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2024-05-29 18:35:44 +0900

    팝업창  메시지 home 으로 전송, home 에서 처리 로직

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,22 +1,44 @@
 import GoToMain from '../components/goToMain';
+import { useEffect } from 'react';
 
 export default function Home() {
-    return (
-        <div className="bg-cover bg-center bg-home-bg w-screen h-screen flex justify-center items-center">
-            <div className="w-full h-full flex justify-center items-center">
-                <div className="flex flex-col w-[972px] h-[611px] flex-shrink-0 font-pretendard text-center">
-                    <div>
-                        <div className='text-home-div font-normal'>
-                            당신의<span className='font-semibold'> 취향 저격 향수</span>를
-                        </div>
-                        <div className='text-home-div font-normalfont-semibold mt-8'>
-                            <p>찾아드릴게요!</p>
-                        </div>
-                    </div>
-                    <div className='mt-[92px] text-home-sub text-subtitle1 font-pretendard'>간단한 테스트로 나에게 어울리는 향수를 추천 받아 보세요.</div>
-                    <GoToMain/>
-                </div>
+  useEffect(() => {
+    const handleLogin = (event: MessageEvent) => {
+      if (
+        event.data === 'loginSuccess' &&
+        event.origin === window.location.origin
+      ) {
+        // 메인 페이지로 이동
+        window.location.href = '/main';
+      }
+    };
+    // 메시지 수신 등록
+    window.addEventListener('message', handleLogin);
+
+    // 메시지 수신 해제
+    return () => {
+      window.removeEventListener('message', handleLogin);
+    };
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-center bg-cover bg-home-bg">
+      <div className="flex items-center justify-center w-full h-full">
+        <div className="flex flex-col w-[972px] h-[611px] flex-shrink-0 font-pretendard text-center">
+          <div>
+            <div className="font-normal text-home-div">
+              당신의<span className="font-semibold"> 취향 저격 향수</span>를
             </div>
+            <div className="mt-8 text-home-div font-normalfont-semibold">
+              <p>찾아드릴게요!</p>
+            </div>
+          </div>
+          <div className="mt-[92px] text-home-sub text-subtitle1 font-pretendard">
+            간단한 테스트로 나에게 어울리는 향수를 추천 받아 보세요.
+          </div>
+          <GoToMain />
         </div>
-    );
-};
+      </div>
+    </div>
+  );
+}
