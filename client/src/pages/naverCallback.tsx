a585be7c967c43296fb0865cbd5d67b267024c17
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2024-05-31 08:22:24 +0900

    jwt 토큰 받는 로직

--- client/src/pages/naverCallback.tsx
+++ client/src/pages/naverCallback.tsx
@@ -1,49 +1,34 @@
 import { useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import apiUrl from '../config';
 
 export default function NaverCallback() {
-  const nav = useNavigate();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const url = new URL(window.location.href);
-    const code = new URL(window.location.href).searchParams.get('code');
-    const state = new URL(window.location.href).searchParams.get('state');
+    const code = url.searchParams.get('code');
+    const state = url.searchParams.get('state');
     console.log('Authorization code:', code);
     console.log('State:', state);
 
     if (code) {
-      const clientId = apiUrl.clientID;
-      const clientSecret = apiUrl.clientSecret;
-      const redirectUri = encodeURI(apiUrl.apiUrl + 'naver-callback');
-
-      const tokenUrl = `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUri}&code=${code}&state=${state}`;
-
       axios
-        .get(tokenUrl)
+        .post('https://perfume-bside.site/api/auth/naver', { code, state })
         .then((response) => {
-          const accessToken = response.data.access_token;
-          console.log('Access token:', accessToken);
-          axios
-            .post('https://perfume-bside.site/api/auth/naver', { accessToken })
-            .then(() => {
-              window.opener.postMessage('loginSuccess', window.location.origin);
-              window.close();
-            })
-            .catch((error) => {
-              console.error('Error sending access token to backend:', error);
-              window.opener.postMessage('loginFail', window.location.origin);
-              window.close();
-            });
+          console.log('Access token received:', response.data.access_token);
+          localStorage.setItem('accessToken', response.data.access_token);
+          navigate('/main');
         })
         .catch((error) => {
-          console.error('Error getting access token:', error);
-          window.opener.postMessage('loginFail', window.location.origin);
-          window.close();
+          console.error('Error during authentication:', error);
+          navigate('/login');
         });
+    } else {
+      console.error('No authorization code found');
+      navigate('/login');
     }
-  }, [nav]);
+  }, [navigate]);
 
   return <div>네이버 로그인 처리 중...</div>;
 }
