2d3a678f08be147840c5bb5c9af317f739627523
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2024-05-31 11:13:31 +0900

    네이버로그인 x

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   apiUrl: import.meta.env.VITE_API_URL,
-  clientID: import.meta.env.VITE_CLIENT_ID,
-  clientSecret: import.meta.env.VITE_CLIENT_SECRET,
+  // clientID: import.meta.env.VITE_CLIENT_ID,
+  // clientSecret: import.meta.env.VITE_CLIENT_SECRET,
 };
 
 export default config;
--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -2,8 +2,8 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
-  readonly VITE_CLIENT_ID: string;
-  readonly VITE_CLIENT_SECRET: string;
+  // readonly VITE_CLIENT_ID: string;
+  // readonly VITE_CLIENT_SECRET: string;
 }
 
 interface ImportMeta {
