fd56fe7e98a2a81012f501ea7d5ef55446f827d5
insidesjsj <tjdwns428@naver.com>
2024-06-03 19:22:20 +0900

    ids 조인

--- client/src/api/getSavedCheck.tsx
+++ client/src/api/getSavedCheck.tsx
@@ -2,9 +2,10 @@ import axiosInstance from './axiosConfig';
 
 export const getSavedCheck = async (ids: number[]) => {
     try {
+        const joinedIds = ids.join(',');
         console.log('getSavedCheck ids: ', ids);
         const response = await axiosInstance.get(`/api/myPage/perfumes/check`,{
-            params: { ids },
+            params: { ids: joinedIds },
         });
         return response.data;
     } catch (error) {
