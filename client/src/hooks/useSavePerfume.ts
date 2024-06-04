ec1b3d7f2922b8d668f6dc1e23ad743de4a349c5
insidesjsj <tjdwns428@naver.com>
2024-06-04 19:07:48 +0900

    create: useSavePerfume 커스텀 훅 생성

---
+++ client/src/hooks/useSavePerfume.ts
@@ -0,0 +1,38 @@
+import {useState} from 'react';
+import {getSavedCheck} from '../api/getSavedCheck';
+import {saveMyPerfume} from '../api/saveMyPerfume';
+
+export const useSavePerfume = (ids: number[]) => {
+    const [saveAlert, setSaveAlert] = useState(false);
+    const [perfumesSaved, setPerfumesSaved] = useState();
+
+    const savedCheck = async (ids: number[]) => {
+        try {
+            const isSaved = await getSavedCheck(ids);
+            setPerfumesSaved(isSaved);
+            console.log('result page perfumesSaved: ', perfumesSaved)
+        } catch (error) {
+            console.error("Error fetching saved check:", error);
+        }
+    };
+    const saveClick = async (id: number, event: React.MouseEvent<HTMLDivElement>) => {
+        console.log('click id: ', id);
+        event.stopPropagation(); // 이벤트 전파 중단
+        try {
+            await saveMyPerfume(id);
+            await savedCheck(ids);
+            setSaveAlert(true);
+            setTimeout(() => {
+                setSaveAlert(false);
+            }, 2000);
+        } catch (error) {
+            console.error("Error saving perfume:", error);
+        }
+    };
+    return {
+        saveAlert,
+        perfumesSaved,
+        saveClick,
+        savedCheck,
+    };
+};
