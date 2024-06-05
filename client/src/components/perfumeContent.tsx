63726db8cd1ef4328d922f29af2014d6847dc7b5
insidesjsj <tjdwns428@naver.com>
2024-06-06 01:43:13 +0900

    modify: perfumeContent에 clova 설명 값 전달

--- client/src/components/perfumeContent.tsx
+++ client/src/components/perfumeContent.tsx
@@ -1,12 +1,17 @@
 import polygon from '../assets/icons/polygon.png';
-const PerfumeContent = () => {
+import {FC} from 'react';
+
+interface PerfumeContentProps {
+    content: string;
+}
+
+const PerfumeContent: FC<PerfumeContentProps> = ({ content }) => {
     return (
         <div className='absolute z-50 ml-[241px]'>
             <div className='flex items-center'>
                 <img src={polygon}/>
                 <div className='flex items-center rounded-[10px] bg-chatGPT py-[22px] px-[43px] min-w-[520px] max-w-[520px] min-h-[126px]'>
-                    달콤 씁쓸하고 살짝 무게감 있는 오렌지 향에
-                    약간의 산도 높은 자몽 향이 섞인 매력적인 향수예요!
+                    {content}
                 </div>
             </div>
         </div>
