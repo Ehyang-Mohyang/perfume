9518d9370ebd27b10d2c32238b97d7e6ac4bd71a
insidesjsj <tjdwns428@naver.com>
2024-06-06 02:37:35 +0900

    modify: perfumeContent 창 닫히는 거 방지

--- client/src/components/perfumeContent.tsx
+++ client/src/components/perfumeContent.tsx
@@ -14,7 +14,7 @@ const PerfumeContent: FC<PerfumeContentProps> = ({ content }) => {
     ));
 
     return (
-        <div className='absolute z-50 ml-[241px]'>
+        <div className='absolute z-50 ml-[241px]' onClick={(event)=>event.stopPropagation()}>
             <div className='flex items-center'>
                 <img src={polygon}/>
                 <div className='flex items-center rounded-[10px] bg-chatGPT py-[22px] px-[43px] min-w-[520px] max-w-[520px] min-h-[126px]'>
