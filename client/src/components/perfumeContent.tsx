514719e3b1d324135a5587424ca3d18f064cbead
insidesjsj <tjdwns428@naver.com>
2024-06-06 02:33:38 +0900

    modify: 향수 설명 문단 나누기

--- client/src/components/perfumeContent.tsx
+++ client/src/components/perfumeContent.tsx
@@ -6,12 +6,19 @@ interface PerfumeContentProps {
 }
 
 const PerfumeContent: FC<PerfumeContentProps> = ({ content }) => {
+    const formattedContent = content.split('\n').map((str, index) => (
+        <>
+            {str}
+            <br />
+        </>
+    ));
+
     return (
         <div className='absolute z-50 ml-[241px]'>
             <div className='flex items-center'>
                 <img src={polygon}/>
                 <div className='flex items-center rounded-[10px] bg-chatGPT py-[22px] px-[43px] min-w-[520px] max-w-[520px] min-h-[126px]'>
-                    {content}
+                    {formattedContent}
                 </div>
             </div>
         </div>
