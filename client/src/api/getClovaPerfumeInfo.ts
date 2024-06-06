0343dea28b1a445b82f42c7acff9ff3197f14a30
insidesjsj <tjdwns428@naver.com>
2024-06-06 21:44:24 +0900

    modify: 스트림 처리 방식 변경

--- client/src/api/getClovaPerfumeInfo.ts
+++ client/src/api/getClovaPerfumeInfo.ts
@@ -11,29 +11,32 @@ export const getClovaPerfumeInfo = async (id: number) => {
         throw error;
     }
 };*/
-export const getClovaPerfumeInfo = async (id: number) => {
-    try {
-        console.log('getClovaPerfumeInfo id: ', id);
-        const response = await fetch(`https://perfume-bside.site/api/clova/perfume/${id}/explanation`, {
-            headers: { 'Content-Type': 'application/json' },
-            credentials: 'include',
-        });
-        const reader = response.body?.getReader();
-        const decoder = new TextDecoder();
-        let result = '';
+export const getClovaPerfumeInfo = (id: number, onData: (data: string) => void) => {
+    const fetchData = async () => {
+        try {
+            console.log('getClovaPerfumeInfo id: ', id);
+            const response = await fetch(`https://perfume-bside.site/api/clova/perfume/${id}/explanation`, {
+                headers: { 'Content-Type': 'application/json' },
+                credentials: 'include',
+            });
+            const reader = response.body?.getReader();
+            const decoder = new TextDecoder();
+            let result = '';
 
-        while (true) {
-            const { done, value } = await reader?.read() || {};
-            if (done) break;
-            result += decoder.decode(value, { stream: true });
+            while (true) {
+                const { done, value } = await reader?.read() || {};
+                if (done) break;
+                result += decoder.decode(value, { stream: true });
+                onData(result);
+            }
+
+            const parsedResult = JSON.parse(result);
+            const content = parsedResult.result.message.content;
+            return content;
+        } catch (error) {
+            console.error('Error getClovaPerfumeInfo', error);
+            throw error;
         }
-
-        const parsedResult = JSON.parse(result);
-        const content = parsedResult.result.message.content;
-        console.log('Content', content);
-        return content;
-    } catch (error) {
-        console.error('Error getClovaPerfumeInfo', error);
-        throw error;
-    }
+    };
+    fetchData();
 };
